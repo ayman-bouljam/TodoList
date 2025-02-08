869eab30e586251cfd86248a4414dfb894744ab4
Aymane <its.aymane.bouljam@gmail.com>
2025-02-08 20:28:26 +0100

    Adding Bootstrap CDN

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-    echo "test";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -7,18 +6,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To-Do List</title>
-    <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
-</head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    </head>
 <body>
   <div class="container mt-5">
-    
+        <h1 class="text-center bg-info">To Do list by Aymane</h1>
   </div>  
+    <form action="index.php" method="post" class="mb-4">
+        <div class="input-group">
+            <input type="text" name="teskName" class="form-control" placeholder="New Task ..." require />
+            <button type="reset" class="btn btn-outline-dark">Add</button>
+        </div>
+    </form>
 
 
 
 
 
-
-    <script src="/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-</body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </body>
 </html>
