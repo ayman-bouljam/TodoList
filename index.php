5a58247e490b4f1d34fe571e6299bdd1b3a031f4
Aymane <its.aymane.bouljam@gmail.com>
2025-02-10 18:43:28 +0100

    add NavBar

--- index.php
+++ index.php
@@ -37,6 +37,27 @@ $deleted_tasks = $conn->query("SELECT * FROM tasks Where task_deleted = 1");
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">Todo List</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Log in</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Sign up</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
   <div class="container mt-5 p-4 bg-light">
         <h1 class="text-center">To Do list by Aymane</h1>
     <form action="index.php" method="POST"  class="mb-4">
